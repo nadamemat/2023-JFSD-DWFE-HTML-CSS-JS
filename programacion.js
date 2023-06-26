4768aadb25cc094324f3af13ba66b27c71651a20
nadamemat <marcos.sagula@gmail.com>
2023-06-25 22:35:49 -0300

    javascript

---
+++ programacion.js
@@ -0,0 +1,10 @@
+function aumentarFoto(identificador){
+    let foto= document.getElementById(identificador);
+    foto.style.width="28.76em";
+    foto.style.float="inline-end";
+}
+function disminuirFoto(identificador){
+    let foto= document.getElementById(identificador);
+    foto.style.width="14.38em";
+    foto.style.float="right";
+}
